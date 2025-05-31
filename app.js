dd26153feeaa181d2f803e7077d13f6385447c99
Sebastián <bichos1506@gmail.com>
2025-05-30 22:49:17 -0600

    proyecto terminado

---
+++ app.js
@@ -0,0 +1,35 @@
+import "bootstrap";
+import "./style.css";
+
+
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
+
+window.onload = function() {
+  //write your code here
+
+  document.getElementById ('btn').addEventListener('click',()=> {
+    document.getElementById ('excuse').innerHTML = randomExcuse();
+
+  });
+  console.log("Hello Rigo from the console!");
+
+};
+
+  let randomExcuse = () => {
+
+    let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+    let action = ['ate', 'peed', 'crushed', 'broke'];
+    let what = ['my homework', 'my phone', 'the car'];
+    let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying' ];
+
+    let whoIndex = Math.floor (Math.random() * who.length);
+    let actionIndex = Math.floor (Math.random() * action.length);
+    let whatIndex = Math.floor (Math.random() * what.length);
+    let whenIndex = Math.floor (Math.random() * when.length);
+
+    return who[whoIndex] + " " + action[actionIndex] + " " + what[whatIndex] + " " + when[ whenIndex];
+  };
+
+
+  
